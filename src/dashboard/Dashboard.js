e799dfe6d58bf51e5d3c8b31b684e8461cea5103
nicbongo <nicbongo@protonmail.com>
2020-10-26 16:48:02 -0300

    file structure: dashboard relocated

--- src/dashboard/Dashboard.js
+++ src/dashboard/Dashboard.js
@@ -8,8 +8,8 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { LineChart, Grid } from 'react-native-svg-charts'
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
-import OrdersScreen from './order/Order.js';
-import CustomHeader from './components/customHeader.js';
+import OrdersScreen from '../order/Order.js';
+import CustomHeader from '../components/customHeader.js';
 
 const Tab = createBottomTabNavigator();
 const Drawer = createDrawerNavigator();
@@ -38,12 +38,12 @@ function TabNavigator() {
 
                     if (route.name === 'Home') {
                         iconName = focused
-                            ? require('./assets/icon/home-black.png')
-                            : require('./assets/icon/home.png')
+                            ? require('../assets/icon/home-black.png')
+                            : require('../assets/icon/home.png')
                     } else if (route.name === 'Order') {
                         iconName = focused ?
-                            require('./assets/icon/order-black.png')
-                            : require('./assets/icon/order.png');
+                            require('../assets/icon/order-black.png')
+                            : require('../assets/icon/order.png');
                     }
 
                     // You can return any component that you like here!
