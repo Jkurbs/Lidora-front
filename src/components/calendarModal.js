93591bf0a47d41ec57902faaa3b37af1c513bb47
Scott Smith <scthsm@outlook.com>
2020-11-20 17:28:35 -0700

    a

--- src/components/calendarModal.js
+++ src/components/calendarModal.js
@@ -1,7 +1,7 @@
 
 import React, { Component } from 'react';
 import { Text, View, Image, TouchableOpacity, StyleSheet } from 'react-native';
-
+import AnimatedHideView from 'react-native-animated-hide-view';
 const styles = StyleSheet.create({
 
     orderModal:{
@@ -38,27 +38,29 @@ class CalendarModal extends Component {
     this.setState({ showCalendar: false });
     }
 
-    ///ORDERCALENDAR MODAL
-    OrderCalendar = ({ handleClose, show, children }) => {
-        if(show){
-        return (
-            <View style={styles.orderModal}>           
-                <button
-                onClick={handleClose}
-                >
-                Close
-                </button>
-            </View>
-        );
-        } else{
-            return (<></>)
-        }
-        };
-    ///ORDERCALENDAR MODAL END
+    // ///ORDERCALENDAR MODAL
+    // OrderCalendar = ({ handleClose, show, children }) => {
+    //     if(show){
+    //     return (
+    //         <View style={styles.orderModal}>           
+    //             <button
+    //             onClick={handleClose}
+    //             >
+    //             Close
+    //             </button>
+    //         </View>
+    //     );
+    //     } else{
+    //         return (<></>)
+    //     }
+    //     };
+    // ///ORDERCALENDAR MODAL END
 
     render(){
         return (
+            <AnimatedHideView visible={this.props.show} style={{ backgroundColor: 'red', padding: 10 }}>
             <this.OrderCalendar show={this.state.showCalendar} handleClose={this.hideModal} />
+            </AnimatedHideView>
         )
     }
 }
