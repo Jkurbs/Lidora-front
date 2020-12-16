a011e12e29620fcf127f34f256ec2ed29ca01e4e
Scott Smith <scthsm@outlook.com>
2020-12-15 21:14:59 -0700

    Update verifyModal.js

--- src/components/customerComponents/verifyModal.js
+++ src/components/customerComponents/verifyModal.js
@@ -4,47 +4,46 @@ import { Text, View, StyleSheet, Dimensions, Modal } from 'react-native';
 import AnimatedHideView from 'react-native-animated-hide-view';
 const { width: windowWidth, height: windowHeight } = Dimensions.get("window");
 
-class VerifyModal extends Component {
-    render() {
+function VerifyModal() {
+
         return (
             <AnimatedHideView visible={true} style={styles.container}>
                 <View style={styles.alert}>
-                    <Text>AYY LMAYO</Text>
+                    <Text style={styles.title}>AYY LaooooO</Text>
                 </View>
             </AnimatedHideView>
         )
     }
-}
 
 const styles = StyleSheet.create({
 
     container: {
         backgroundColor: 'rgba(0, 0, 0, 0.1)',
-        justifyContent: 'center',
         height: '100%',
-        width: windowWidth,
+        width: '100%',
         position: 'absolute',
-        alignSelf: 'center',
         zIndex: 99999,
+        justifyContent:'flex-end'
     },
 
     alert: {
         backgroundColor: 'white',
-        borderRadius: 5,
-        width: 550,
+        // borderRadius: 5,
+        width: '100%',
         height: 180,
         alignSelf: 'center',
         padding: 30,
         shadowColor: "#000",
         shadowColor: "#000",
         shadowOpacity: 0.13,
-        shadowRadius: 10.68
+        shadowRadius: 10.68,
+        justifyContent:'center'
     },
 
     title: {
-        fontSize: 25,
+        fontSize: 18,
         fontWeight: '600',
-        marginBottom: 8
+        marginBottom: 8,
     },
 
     secondaryText: {
