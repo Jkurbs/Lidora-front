4f92d0d35fe5c46ee0a481cb8495d07d6f2c6072
Kerby Jean <Kurbs@Kerbys-MacBook-Pro.local>
2020-12-21 13:12:59 -0500

    Added black background

--- src/customerPages/storeFront/storeFront.js
+++ src/customerPages/storeFront/storeFront.js
@@ -1,25 +1,21 @@
-import React, { useState, useRef, useEffect } from "react";
 
-import { View, Image, Text, SectionList, ScrollView, TouchableOpacity, Dimensions, TextInput } from "react-native";
+import React, { useState, useRef, useEffect } from "react";
+import { View, SafeAreaView, Image, Text, SectionList, TouchableOpacity, Dimensions, TextInput, Linking, Animated,} from "react-native";
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import firebase from "../../firebase/Firebase";
 import styles from './storeFront.style'
 import "firebase/firestore";
-
-import ReactPlaceholder from 'react-placeholder';
-import "react-placeholder/lib/reactPlaceholder.css";
+var db = firebase.firestore();
 
 import EmptyBag from '../../components/emptyBagView'
-
 import BottomSheet from 'reanimated-bottom-sheet';
-import { } from "react-native-gesture-handler";
-const { height } = Dimensions.get("window")
+import ReactPlaceholder from 'react-placeholder';
 
-var db = firebase.firestore();
+const { height, width } = Dimensions.get("window")
 
 const FlatListItemSeparator = () => {
     return (
-        //Item Separator
         <View style={styles.listItemSeparatorStyle} />
     );
 };
@@ -51,18 +47,21 @@ function StoreInfoCell(item) {
 }
 
 function StoreFront(props) {
-
     // States 
     const [data, setData] = useState({ user: [] })
     const [menu, setMenu] = useState([])
     const [titles, setTitle] = useState({ headerTitle: "View Bag", LeftButtonTitle: "" })
     const [selectedItem, setSelectedItem] = useState(null)
-    const [order, setOrder] = useState({ quantity: 1, note: "" })
+    const [quantity, setQuantity] = useState(1)
+    const [total, setTotal] = useState(0)
+    const [note, setNote] = useState("")
     const [bag, setBag] = useState([])
-
+    const[isOpen, setIsOpen] = useState(false) 
+    const [opacity, setOpacity] = useState(new Animated.Value(0))
     const [isInnerScrollEnabled, setIsInnerScrollEnabled] = useState(false)
 
-    // Refs 
+    // Refs  
+    const bs = React.createRef(null);
     const sheetRef = useRef(null);
     const scrollRef = useRef(null)
 
@@ -91,35 +90,83 @@ function StoreFront(props) {
         })
     }, [])
 
-
-    const selectItem = (item) => {
-        console.log(item.item.ingredients)
-        setSelectedItem(item.item)
-        sheetRef.current.snapTo(1)
-    }
-
-    const dismissItem = () => {
-        setSelectedItem(null)
-        sheetRef.current.snapTo(0)
-    }
-
-
-    const onOpenEnd = () => {
-        if (scrollRef.current !== null) {
-            setIsInnerScrollEnabled(true)
-            scrollRef.current.forceUpdate()
-        }
-    }
-
-    const onCloseEnd = () => {
-        if (scrollRef.current !== null) {
-            setIsInnerScrollEnabled(false)
-            scrollRef.current.forceUpdate()
+    const calculateTotal = (add) => {
+        if (add) {
+            setQuantity(quantity + 1)
+            const result =  +(total + selectedItem.price).toFixed(2)
+            setTotal(result)
+        } else {
+            if (quantity < 2) {
+                return 
+            }  else {
+                setQuantity(quantity - 1)
+                const result =  +(total - selectedItem.price).toFixed(2)
+                setTotal(result)
+            }
         }
     }
 
 
-    const RenderItemInfos = (item) => (
+const ChefCell = (item) => {
+    return (
+        <View>
+            <ReactPlaceholder color='red' showLoadingAnimation={false} type='rect' ready={item != null} style={{ height: 250, width: '100%' }}>
+                <Image style={styles.storeImage} source={{ uri: item.item.user.imageURL }} />
+            </ReactPlaceholder>
+            <View style={styles.storeInfoContainer}>
+                <Text style={styles.title}>{item.item.user.title}</Text>
+                <Text style={styles.description}>{item.item.user.description}</Text>
+            </View>
+            <View style={styles.listItemSeparatorStyle} />
+        </View>
+    )
+}
+
+const StoreInfoCell= (item) => {
+    return (
+        <View>
+            <View style={{ margin: 20, marginTop: 10, marginBottom: 10 }}>
+                <Text style={styles.description}>{item.item.user.city}, {item.item.user.state}</Text>
+                <Text style={styles.description}>{item.item.user.email_address}</Text>
+            </View>
+            <View style={styles.listItemSeparatorStyle} />
+        </View>
+    )
+}
+
+    const MenuCell = (item) => {
+        return (
+            <TouchableOpacity onPress={()=>onOpen(item)} >
+                <View style={{ alignItems: 'center', margin: 20, flexDirection: 'row', justifyContent: 'space-between' }}>
+                    <View style={{ flexDirection: 'column', justifyContent: 'space-between', width: '70%' }}>
+                        <Text style={styles.menuName}>{item.item.name}</Text>
+                        <Text style={styles.menuDescription}>{item.item.description}</Text>
+                        <Text style={styles.menuPrice}>${item.item.price}</Text>
+                    </View>
+                    <Image style={styles.menuImage} source={{
+                        uri: item.item.imageURL,
+                    }} />
+                </View>
+            </TouchableOpacity>
+        )
+    }
+
+      const renderHeader = () => (
+        <View style={styles.header}>
+            <TouchableOpacity onPress={ onClose } style={{ justifyContent: 'center', position: 'absolute', left: 16 }}>
+                <Text style={{ color: '#34C759', fontSize: 17, fontWeight: '500', alignSelf: 'center' }}>{titles.LeftButtonTitle}</Text>
+            </TouchableOpacity>
+            <Text style={styles.headerTitle} onPress={()=>onOpen()}>{titles.headerTitle}</Text>
+            <View style={[styles.listItemSeparatorStyle, { position: 'absolute', bottom: 0 }]} />
+        </View>
+    )
+
+
+    const RenderIngredientsItem = (item) => (
+        <Text style={{ margin: 8, padding: 8, fontWeight: '500', fontSize: 15 }}>{item.item?.name ?? ""}</Text>
+    );
+
+        const RenderItemInfos = (item) => (
         <View style={styles.itemDescriptionContainer}>
             <View style={{ flexDirection: 'column', maxWidth: '50%' }}>
                 <Text style={styles.menuName}>{item.item?.name ?? ""}</Text>
@@ -132,13 +179,9 @@ function StoreFront(props) {
         </View>
     );
 
-
-    const RenderIngredientsItem = (item) => (
-        <Text style={{ margin: 8, padding: 8, fontWeight: '500', fontSize: 15 }}>{item.item?.name ?? ""}</Text>
-    );
-
-    const RenderInstructions = () => (
-        <View>
+        const RenderInstructions = () => (
+        
+        <View style={{ flex: 1, marginBottom: 50}}>
             <TextInput
                 style={{ height: 60, padding: 20 }}
                 placeholder={"Add a note (Extra sauce, no salt, etc.)"}
@@ -156,34 +199,15 @@ function StoreFront(props) {
         </View>
     )
 
+      const renderContent = () => {
 
-
-    const MenuCell = (item) => {
-        return (
-            <TouchableOpacity onPress={() => { selectItem(item) }} >
-                <View style={{ alignItems: 'center', margin: 20, flexDirection: 'row', justifyContent: 'space-between' }}>
-                    <View style={{ flexDirection: 'column', justifyContent: 'space-between', width: '70%' }}>
-                        <Text style={styles.menuName}>{item.item.name}</Text>
-                        <Text style={styles.menuDescription}>{item.item.description}</Text>
-                        <Text style={styles.menuPrice}>${item.item.price}</Text>
-                    </View>
-                    <Image style={styles.menuImage} source={{
-                        uri: item.item.imageURL,
-                    }} />
-                </View>
-            </TouchableOpacity>
-        )
-    }
-
-    const renderContent = () => {
         if (selectedItem === null) {
             return <EmptyBag />
         } else {
             return (
                 <View style={styles.bagContainer}>
                     <SectionList
-                        ref={scrollRef}
-                        style={{}}
+                        style={{borderTopLeftRadius: 5, borderTopRightRadius: 5, marginTop: 30, paddingBottom: '70%'}}
                         keyExtractor={(item, index) => item + index}
                         sections={[
                             { title: "Infos", data: [selectedItem] },
@@ -214,96 +238,144 @@ function StoreFront(props) {
                                     break;
                             }
                         }}
-                        pointerEvents={isInnerScrollEnabled === true ? 'auto' : 'none'}
-                        scrollEnabled={isInnerScrollEnabled}
                         ItemSeparatorComponent={FlatListItemSeparator}
                         stickySectionHeadersEnabled={false}
                     />
-
                     <View style={styles.listItemSeparatorStyle} />
-
-
                 </View>
             )
         }
     }
 
-    const renderHeader = () => (
-        <View style={styles.header}>
-            <TouchableOpacity onPress={() => { dismissItem() }} style={{ justifyContent: 'center', position: 'absolute', left: 8 }}>
-                <Text style={{ color: '#34C759', fontSize: 15, fontWeight: '500' }}>{titles.LeftButtonTitle}</Text>
-            </TouchableOpacity>
-            <Text style={styles.headerTitle} onPress={() => { sheetRef.current.snapTo(1) }}>{titles.headerTitle}</Text>
-            <View style={[styles.listItemSeparatorStyle, { position: 'absolute', bottom: 0 }]} />
-        </View>
-    )
+  const onClose = () => {
+    setSelectedItem(null)
+    Animated.timing(opacity, {
+      toValue: 0,
+      duration: 350,
+      useNativeDriver: true,
+    }).start();
+    bs.current.snapTo(0);
+    setTimeout(() => {
+        setIsOpen(false)
+    }, 50);
+  };
 
-    // Fetch chef
+  const onOpen = (item) => {
+    setTimeout(() => {
+        
+    }, 50);
+    setIsOpen(true)
+    setQuantity(1)
+    setTotal(item.item.price)
+    setSelectedItem(item?.item ?? null)
+    bs.current.snapTo(1);
+    Animated.timing(opacity, {
+      toValue: 0.7,
+      duration: 300,
+      useNativeDriver: true,
+    }).start();
+  };
+
+
+  const renderBackDrop = () => (
+    <Animated.View
+      style={{
+        opacity: opacity,
+        backgroundColor: '#000',
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+      }}>
+      <TouchableOpacity
+        style={{
+          width: width,
+          height:height,
+          backgroundColor: 'transparent',
+        }}
+        activeOpacity={1}
+        onPress={onClose}
+      />
+    </Animated.View>
+  );
+
     return (
-        <View style={styles.container}>
-            <View>
-                <SectionList
-                    style={{ height: height, paddingBottom: 120 }}
-                    keyExtractor={(item, index) => item + index}
-                    sections={[
-                        // homogenous rendering between sections
-                        {
-                            title: "user",
-                            data: [data]
-                        },
-                        {
-                            title: "Additional Info",
-                            data: [data],
-                        },
-                        {
-                            title: "Menu",
-                            data: menu
-                        },
-                    ]}
-                    renderSectionHeader={({ section }) => {
-                        if (section.title === "user") { return null } else {
-                            return (
-                                <View style={styles.headerView}>
-                                    <Text style={styles.sectionTitle}>{section.title}</Text>
-                                </View>
-                            );
-                        }
-                    }}
-                    renderItem={({ item, section }) => {
-                        switch (section.title) {
-                            case "user":
-                                return <ChefCell item={item} />
-                            case "Additional Info":
-                                return <StoreInfoCell item={item} />
-                            case "Menu":
-                                return <MenuCell item={item} />
-                            default:
-                                break;
-                        }
-                    }}
-                    ItemSeparatorComponent={FlatListItemSeparator}
-                    stickySectionHeadersEnabled={false}
-                />
-            </View>
-            <BottomSheet
-                ref={sheetRef}
-                initialSnap={0}
-                snapPoints={["10%", "90%"]}
-                // enabledInnerScrolling={true}
-                // enabledGestureInteraction={false}
-                // enabledHeaderGestureInteraction={true}
-                // enabledContentGestureInteraction={false}
-                renderContent={renderContent}
-                renderHeader={renderHeader}
-                onOpenStart={() => setTitle({ headerTitle: "", LeftButtonTitle: "Dismiss" })}
-                onCloseStart={() => setTitle({ headerTitle: "View Bag", LeftButtonTitle: "" })}
-                onOpenEnd={onOpenEnd}
-                onCloseEnd={onCloseEnd}
-            >
+        <SafeAreaProvider>
+            <SafeAreaView style={{flex: 1}} forceInset={{top: 'always'}}>
+            <View style={styles.container}>
+        
+        <SectionList
+            style={{height: height, backgroundColor: 'white', borderTopLeftRadius: 20,
+            borderTopRightRadius: 20 }}
+            keyExtractor={(item, index) => item + index}
 
-            </BottomSheet>
-        </View>
+            sections={[
+                // homogenous rendering between sections
+                {
+                    title: "user",
+                    data: [data]
+                },
+                {
+                    title: "Additional Info",
+                    data: [data],
+                },
+                {
+                    title: "Menu",
+                    data: menu
+                },
+            ]}
+            renderSectionHeader={({ section }) => {
+                if (section.title === "user") { return null } else {
+                    return (
+                        <View style={styles.headerView}>
+                            <Text style={styles.sectionTitle}>{section.title}</Text>
+                        </View>
+                    );
+                }
+            }}
+            renderItem={({ item, section }) => {
+                switch (section.title) {
+                    case "user":
+                        return <ChefCell item={item} />
+                    case "Additional Info":
+                        return <StoreInfoCell item={item} />
+                    case "Menu":
+                        return <MenuCell item={item} />
+                    default:
+                        break;
+                }
+            }}
+
+            scrollEventThrottle={16}
+            refreshing={false}
+            ref={scrollRef}
+            ItemSeparatorComponent={FlatListItemSeparator}
+            stickySectionHeadersEnabled={false}
+        />
+
+{isOpen && renderBackDrop()}
+
+        <BottomSheet
+          ref={bs}
+          snapPoints={["10%", "90%"]}
+          initialSnap={0}
+          renderHeader={renderHeader}
+          renderContent={renderContent}
+          onCloseEnd={onClose}
+          onOpenStart={() => setTitle({ headerTitle: "", LeftButtonTitle: "Dismiss" })}
+        onCloseStart={() => setTitle({ headerTitle: "View Bag", LeftButtonTitle: "" })}
+        />
+      </View>
+            </SafeAreaView>
+
+</SafeAreaProvider>
+      
     );
+  
 }
 
 export default StoreFront
+
+
+
